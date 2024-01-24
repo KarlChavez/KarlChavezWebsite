e42051eb1124c8f8e8565f1cb930f3b59e9713ba
KarlChavez <zevahck@gmail.com>
2024-01-23 22:49:25 -0500

    Edited the About and My Experience Section
    Adding my own information

--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,23 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+        Coding has always been a passion for me since high school, but it wasn’t
+        until my sophomore year of college that I specifically narrowed my
+        expertise, which is web development. After doing some freelance work by
+        creating free websites for friends and neighbors, I enrolled in a coding
+        boot camp and learned front-end development. Later, I used my skills in
+        an internship at Bass Pro Shops. I am currently a Front-end Developer
+        Intern but am{" "}
+        <span className="underline">
+          looking for a full-time position in front-end or full-stack web
+          development after graduating in May 2024.
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="italic">When I'm not coding</span>, I enjoy lifting
+        weights, hiking, planning my next trip, and spending time with family
+        and friends :) 🏋️🥾✈️👪
       </p>
     </motion.section>
   );
--- lib/data.ts
+++ lib/data.ts
@@ -35,28 +35,36 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Freelance Web Developer",
+    location: "Remote",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "During my sophomore year of college, I decided to explore web development. I began by watching YouTube videos about HTML, CSS, and JavaScript to create basic websites. Then, I opened a Fiverr account, a freelance service marketplace, and crafted websites for friends and neighbors for free!",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2022 (July - Aug)",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Front-End Bootcamp",
+    location: "Remote",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      'I loved creating websites over the summer, so I decided to buy a Front-end bootcamp from Scrimba called "The Front-End Developer Career Path." It includes 70+ hours of tutorials, hundreds of coding challenges, and dozens of real-world projects using HTML, CSS, JS, React.js, Firebase, Responsive Design, and many more web tools.',
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2022 - 2023 (Nov - July)",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Front-end Developer Internship at Bass Pro Shops",
+    location: "Springfield, MO",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I worked full-time with the Buying Team, responsible for the checkout/shopping cart and payment system on the Bass Pro Shop e-commerce website. I extensively utilized React.js, Next.js, Recoil (for state management), SCSS, and TypeScript.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2023 (May - Aug)",
+  },
+  {
+    title: "Continued Front-end Developer Internship at Bass Pro Shops",
+    location: "Remote",
+    description:
+      "I currently work part-time remotely with the Shopping Team, focusing on the 'shopping aspect' of a website. This involves tasks such as browsing through items, enhancing search functionality, and curating a selection of items tailored to meet customer needs. I am currently working part-time as I finish my last semester as a full-time student",
+    icon: React.createElement(FaReact),
+    date: "2023 - Present (Aug - )",
   },
 ] as const;
 
