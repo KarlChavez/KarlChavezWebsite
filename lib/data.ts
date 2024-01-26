8e32d30592a9edcdfa7308d1dd77deb8ec3f5155
KarlChavez <zevahck@gmail.com>
2024-01-25 23:40:42 -0500

    Added and Replaced Images

--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,12 @@
 import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { MdOutlineWorkHistory } from "react-icons/md";
 import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import { RiComputerLine } from "react-icons/ri";
+import ProductDetailPageBassProShops from "@/public/ProductDetailPageBassProShops.jpg";
+import CheckoutPageBassProShops from "@/public/CheckoutPageBassProShops.jpg";
+import HistoryEvents from "@/public/HistoryEvents.jpg";
+import CodeToImage from "@/public/CodeToImage.jpg";
 
 export const links = [
   {
@@ -16,16 +18,16 @@ export const links = [
     hash: "#about",
   },
   {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
     name: "Projects",
     hash: "#projects",
   },
   {
     name: "Skills",
     hash: "#skills",
-  },
-  {
-    name: "Experience",
-    hash: "#experience",
   },
   {
     name: "Contact",
@@ -39,7 +41,7 @@ export const experiencesData = [
     location: "Remote",
     description:
       "During my sophomore year of college, I decided to explore web development. I began by watching YouTube videos about HTML, CSS, and JavaScript to create basic websites. Then, I opened a Fiverr account, a freelance service marketplace, and crafted websites for friends and neighbors for free!",
-    icon: React.createElement(LuGraduationCap),
+    icon: React.createElement(RiComputerLine),
     date: "2022 (July - Aug)",
   },
   {
@@ -47,7 +49,7 @@ export const experiencesData = [
     location: "Remote",
     description:
       'I loved creating websites over the summer, so I decided to buy a Front-end bootcamp from Scrimba called "The Front-End Developer Career Path." It includes 70+ hours of tutorials, hundreds of coding challenges, and dozens of real-world projects using HTML, CSS, JS, React.js, Firebase, Responsive Design, and many more web tools.',
-    icon: React.createElement(CgWorkAlt),
+    icon: React.createElement(FaReact),
     date: "2022 - 2023 (Nov - July)",
   },
   {
@@ -55,7 +57,7 @@ export const experiencesData = [
     location: "Springfield, MO",
     description:
       "I worked full-time with the Buying Team, responsible for the checkout/shopping cart and payment system on the Bass Pro Shop e-commerce website. I extensively utilized React.js, Next.js, Recoil (for state management), SCSS, and TypeScript.",
-    icon: React.createElement(FaReact),
+    icon: React.createElement(MdOutlineWorkOutline),
     date: "2023 (May - Aug)",
   },
   {
@@ -63,7 +65,7 @@ export const experiencesData = [
     location: "Remote",
     description:
       "I currently work part-time remotely with the Shopping Team, focusing on the 'shopping aspect' of a website. This involves tasks such as browsing through items, enhancing search functionality, and curating a selection of items tailored to meet customer needs. I am currently working part-time as I finish my last semester as a full-time student",
-    icon: React.createElement(FaReact),
+    icon: React.createElement(MdOutlineWorkHistory),
     date: "2023 - Present (Aug - )",
   },
 ] as const;
@@ -73,15 +75,31 @@ export const projectsData = [
     title: "Product Detail Page",
     description:
       "Worked on pricing, availability, reviews, descriptions, and badges to showcase detailed information on SKU items",
-    tags: ["Next.js", "React.js", "Node.js", "TypeScript", "SCSS", "Recoil", "Jest"],
-    imageUrl: corpcommentImg,
+    tags: [
+      "Next.js",
+      "React.js",
+      "Node.js",
+      "TypeScript",
+      "SCSS",
+      "Recoil",
+      "Jest",
+    ],
+    imageUrl: ProductDetailPageBassProShops,
   },
   {
     title: "Checkout Page",
     description:
       "Implemented shipping functionality, including forms, credit cards, gift cards, in-store pickup availability, fees, and other shipping details",
-    tags: ["Next.js", "React.js", "Node.js", "TypeScript", "SCSS", "Recoil", "Jest"],
-    imageUrl: rmtdevImg,
+    tags: [
+      "Next.js",
+      "React.js",
+      "Node.js",
+      "TypeScript",
+      "SCSS",
+      "Recoil",
+      "Jest",
+    ],
+    imageUrl: CheckoutPageBassProShops,
   },
   {
     title: "Code To Image",
@@ -95,20 +113,14 @@ export const projectsData = [
       "Tailwind",
       "Firebase",
     ],
-    imageUrl: wordanalyticsImg,
+    imageUrl: CodeToImage,
   },
   {
     title: "Geolocation History Events",
     description:
       "Developed a map application with local storage to save favorited historic locations, providing users with trip ideas",
-    tags: [
-      "Next.js",
-      "React.js",
-      "Node.js",
-      "TypeScript",
-      "Tailwind",
-    ],
-    imageUrl: wordanalyticsImg,
+    tags: ["Next.js", "React.js", "Node.js", "TypeScript", "Tailwind"],
+    imageUrl: HistoryEvents,
   },
 ] as const;
 
